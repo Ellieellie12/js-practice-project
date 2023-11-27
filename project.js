1f85e08ae4492d6882e69fa735600a9c7d64f2b7
Ellie <eagyeman1234@gmail.com>
2023-11-27 17:43:28 -0500

    add getBet function

--- project.js
+++ project.js
@@ -22,11 +22,38 @@ const deposit = () => {
     }
   }
 }
+// while loop continues until the conditionals are met if user types an input that's not a number they will get the message that states invalid and they will get enter a deposit amount until the input is fulfilled properly which in this case a a positive number would need to be entered. (above function ^)
 
 
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ")
+    const numberOfLines = parseFloat(lines)
+  
+    if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log('Invalid number of lines, try again') 
+    } else {
+      return numberOfLines
+    }
+  }
+}
 
 
-const depositAmount = deposit()
+const getBet = (balance, lines) => {
+  while (true) {
+    const bet = prompt("Enter the bet per line: ")
+    const numberBet = parseFloat(bet)
+  
+    if(isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log('Invalid bet, try again') 
+    } else {
+      return numberBet
+    }
+  }
+}
 
 
-//calling the function
+let balance = deposit()
+const numberOfLines = getNumberOfLines()
+const bet = getBet(balance, numberOfLines)
+//calling the function(s)
