88febc5552e01532a2542810dc40a0dbf45a96a9
Ellie <eagyeman1234@gmail.com>
2023-11-27 16:35:55 -0500

    add function for deposits

--- project.js
+++ project.js
@@ -6,3 +6,17 @@
 // 5. Check if the user won
 // 6. Give the user their winnings
 // 7. Allow the user to play again 
+
+const prompt = require("prompt-sync")()
+
+
+const deposit = () => {
+  const depositAmount = prompt("Enter a deposit amount: ")
+  const numberDepositAmount = parseFloat(depositAmount)
+
+  if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+    console.log('Invalid deposit amount, try again')
+  }
+}
+deposit()
+//calling the function
